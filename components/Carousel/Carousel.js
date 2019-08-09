35661590a975b8e63eddf03308d59e206e73b3e5
teddyhn <tngo97@gmail.com>
2019-08-09 10:33:56 -0700

    Completed self study questions

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,51 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function Carousel() {
+
+  let carousel = document.createElement('div');
+  carousel.classList.add('carousel');
+
+  let leftButton = document.createElement('div');
+  leftButton.classList.add('left-button');
+  leftButton.textContent = ' < ';
+
+  let img1 = document.createElement('img');
+  img1.src = './assets/carousel/mountains.jpeg'
+  img1.classList.add('active');
+  let img2 = document.createElement('img');
+  img2.src = './assets/carousel/computer.jpeg'
+  let img3 = document.createElement('img');
+  img3.src = './assets/carousel/trees.jpeg'
+  let img4 = document.createElement('img');
+  img4.src = './assets/carousel/turntable.jpeg'
+
+  const images = [img1, img2, img3, img4];
+
+  let rightButton = document.createElement('div');
+  rightButton.classList.add('right-button');
+  rightButton.textContent = ' > ';
+  rightButton.addEventListener('click', evt => {
+    for (i = 0; i <= images.length; i++) {
+      if (images[i].classList.contains('active')) {
+        images[i].classList.toggle('active');
+        images[i+1].classList.add('active')
+      }
+    }
+  })
+
+  carousel.appendChild(leftButton)
+  carousel.appendChild(img1)
+  carousel.appendChild(img2)
+  carousel.appendChild(img3)
+  carousel.appendChild(img4)
+  carousel.appendChild(rightButton)
+
+  let carouselContainer = document.querySelector('.carousel-container');
+  carouselContainer.appendChild(carousel);
+
+  return carousel;
+}
+
+Carousel();
